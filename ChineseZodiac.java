32e5e9eef6c72d049666407111c8eac621dac514
toojessy <jnkamienski@my.waketech.edu>
2025-09-09 10:18:32 -0400

    Update ChineseZodiac.java

--- ChineseZodiac.java
+++ ChineseZodiac.java
@@ -1,10 +1,13 @@
 // Jessica Kamienski
 
-import java.util.Scanner
+import java.util.Scanner;
 
 class ChineseZodiac {
     public static void main(String[] args) {
         System.out.println("Enter a year: ");
+        Scanner input = new Scanner(System.in);
+        int year = input.nextInt();
+        // you want an integer
         
         final int ZODIAC_MONKEY = 0;
         final int ZODIAC_ROOSTER = 1;
@@ -18,5 +21,24 @@ class ChineseZodiac {
         final int ZODIAC_SNAKE = 9;
         final int ZODIAC_HORSE = 10;
         final int ZODIAC_SHEEP = 11;
+        
+       
+        switch (year % 12) {
+            // mod the year by 12, the number you get as the
+            // result is what you use to compare
+            case 0;
+            System.out.println("Monkey");
+            // does this help?
+            break;
+            
+            case 1;
+            System.out.println("Rooster");
+            break;
+            
+            case 2;
+            System.out.println("Dog")
+            break;
+                
+        }
     }
 }
