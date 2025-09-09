499877931c9872db3e04cee809e4cce66b333513
toojessy <jnkamienski@my.waketech.edu>
2025-09-09 10:04:27 -0400

    Update ChineseZodiac.java

--- ChineseZodiac.java
+++ ChineseZodiac.java
@@ -1,1 +1,22 @@
+// Jessica Kamienski
 
+import java.util.Scanner
+
+class ChineseZodiac {
+    public static void main(String[] args) {
+        System.out.println("Enter a year: ");
+        
+        final int ZODIAC_MONKEY = 0;
+        final int ZODIAC_ROOSTER = 1;
+        final int ZODIAC_DOG = 2;
+        final int ZODIAC_PIG = 3;
+        final int ZODIAC_RAT = 4;
+        final int ZODIAC_OX = 5;
+        final int ZODIAC_TIGER = 6;
+        final int ZODIAC_RABBIT = 7;
+        final int ZODIAC_DRAGON = 8;
+        final int ZODIAC_SNAKE = 9;
+        final int ZODIAC_HORSE = 10;
+        final int ZODIAC_SHEEP = 11;
+    }
+}
