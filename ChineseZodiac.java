5834ea99226ca25c295a20c7f5a0295fdf880c22
toojessy <jnkamienski@my.waketech.edu>
2025-09-09 20:16:41 -0400

    Update ChineseZodiac.java

--- ChineseZodiac.java
+++ ChineseZodiac.java
@@ -26,19 +26,54 @@ class ChineseZodiac {
         switch (year % 12) {
             // mod the year by 12, the number you get as the
             // result is what you use to compare
-            case 0;
+            case 0:
             System.out.println("Monkey");
             // does this help?
             break;
             
-            case 1;
+            case 1:
             System.out.println("Rooster");
             break;
             
-            case 2;
-            System.out.println("Dog")
+            case 2:
+            System.out.println("Dog");
             break;
-                
+            
+            case 3:
+            System.out.println("Pig");
+            break;
+            
+            case 4:
+            System.out.println("Rat");
+            break;
+            
+            case 5:
+            System.out.println("Ox");
+            break;
+            
+            case 6:
+            System.out.println("Tiger");
+            break;
+            
+            case 7:
+            System.out.println("Rabbit");
+            break;
+            
+            case 8:
+            System.out.println("Dragon");
+            break;
+            
+            case 9:
+            System.out.println("Snake");
+            break;
+            
+            case 10:
+            System.out.println("Horse");
+            break;
+            
+            case 11:
+            System.out.println("Sheep");
+            break;
         }
     }
 }
